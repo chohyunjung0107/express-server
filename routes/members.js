38fd94360e3268aea40d587101c34384d83ee6dc
hyunjung <chogodot@naver.com>
2021-08-06 20:48:31 +0900

    Revert "Initial commit"
    This reverts commit f0ed81c5e0f1437599a1a801d6f2d59ea23515d3.

--- routes/members.js
+++ routes/members.js
@@ -1,54 +1,68 @@
 const router = global.express.Router();
 const members = global.mocks.members;
-const jwtAuth = require('../middlewares/jwtAuth.js');
-
+const jwtAuth = require("../middlewares/jwtAuth.js");
 
 //로그인 api
-router.post('/login/', function(request, response) {
-  // TODO: 로그인 가능한 회원인지 확인
-  jwtAuth.tokenCreate(request, response, request.body);
+router.post("/login/", function (request, response) {
+	// TODO: 로그인 가능한 회원인지 확인
+	let searchMember;
+	for (let index = 0; index < members.length; index++) {
+		if (
+			members[index].id === request.body.id &&
+			members[index].password === request.body.password
+		) {
+			searchMember = members[index];
+		}
+	}
+	if (searchMember) {
+		jwtAuth.tokenCreate(request, response, request.body);
+	} else {
+		response.status(403).json({
+			message: "Wrong id or password",
+		});
+	}
 });
 
 //미들웨어는 경로와 함수 사이에 넣어준다.
-router.get('/login/', jwtAuth.tokenCheck, function(request, response) {
-  response.status(200).send({
-    decoded: request.decoded
-  });
+router.get("/login/", jwtAuth.tokenCheck, function (request, response) {
+	response.status(200).send({
+		decoded: request.decoded,
+	});
 });
 
 //멤버스 api
-router.post('/', function(request, response) {
-  members.push(request.body);
-  console.log('Done members post', members);
-  response.status(200).send({
-    result: 'Created'
-  });
+router.post("/", function (request, response) {
+	members.push(request.body);
+	console.log("Done members post", members);
+	response.status(200).send({
+		result: "Created",
+	});
 });
 
-router.get('/', function(request, response) {
-  console.log('Done members get', members);
-  //요청하고 200(정상)응답
-  response.status(200).send({
-    result: 'Readed',
-    members: members
-  });
+router.get("/", function (request, response) {
+	console.log("Done members get", members);
+	//요청하고 200(정상)응답
+	response.status(200).send({
+		result: "Readed",
+		members: members,
+	});
 });
 
-router.patch('/', function(request, response) {
-  members[request.body.index] = request.body.member;
-  console.log('Done members patch', members);
-  response.status(200).send({
-    result: 'Updated'
-  });
+router.patch("/", function (request, response) {
+	members[request.body.index] = request.body.member;
+	console.log("Done members patch", members);
+	response.status(200).send({
+		result: "Updated",
+	});
 });
 
-router.delete('/:index', function(request, response) {
-  const index = Number(request.params.index);
-  members.splice(index, 1);
-  console.log('Done members delete', members);
-  response.status(200).send({
-    result: 'Deleted'
-  });
+router.delete("/:index", function (request, response) {
+	const index = Number(request.params.index);
+	members.splice(index, 1);
+	console.log("Done members delete", members);
+	response.status(200).send({
+		result: "Deleted",
+	});
 });
 
 module.exports = router;
